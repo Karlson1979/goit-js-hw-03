4aed10f2dad2a1f8b00d87ed0419deff69344c0d
Tochka <tochka114@gmail.com>
2024-04-13 13:32:30 +0300

    task-3

--- task-2.js
+++ task-2.js
@@ -1,11 +1,10 @@
-function makeArray(firstArray, secondArray, maxLength ){
-const concatArr=firstArray.concat(secondArray,maxLength);
-if(concatArr.length>maxLength){
-    return concatArr.slice(0,maxLength)
-}
-else{
-    return concatArr
-}
+function makeArray(firstArray, secondArray, maxLength) {
+  const concatArr = firstArray.concat(secondArray, maxLength);
+  if (concatArr.length > maxLength) {
+    return concatArr.slice(0, maxLength);
+  } else {
+    return concatArr;
+  }
 }
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
